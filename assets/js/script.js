eb2a96df70b1435cbddcba9d94d402fd96ee4e4e
Kyle Young <kyleyoung.9210@gmail.com>
2021-02-25 20:26:18 +0000

    Changing hours back to nums and adding data-hour attr to hourTask

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,7 @@
 let currentDay = $("#currentDay");
 let workDayContainer = $(".container");
-let hours = [
-  "9 am",
-  "10 am",
-  "11 am",
-  "12 pm",
-  "13 pm",
-  "14 pm",
-  "15 pm",
-  "16 pm",
-  "17 pm",
-];
-let currentHour = moment().format("H a");
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let currentHour = Number.parseInt(moment().format("H"));
 let timeBlock;
 let hourEl;
 let hourTask;
@@ -24,16 +14,16 @@ function displayTime() {
 }
 displayTime();
 
-function addHourRow(hour) {
+function addHourRow(hour, dataHour) {
   timeBlock = $("<div>").addClass("time-block row");
   workDayContainer.append(timeBlock);
   hourEl = $("<p>")
     .addClass("hour col-2 d-flex align-items-center justify-content-center")
     .text(hour);
   timeBlock.append(hourEl);
-  hourTask = $(
-    "<textarea placeholder='Enter task to be completed...'>"
-  ).addClass("col-8 textarea");
+  hourTask = $("<textarea placeholder='Enter task to be completed...'>")
+    .addClass("col-8 textarea")
+    .attr("data-hour", dataHour);
   timeBlock.append(hourTask);
   saveBtn = $("<button>").addClass("saveBtn col-2");
   timeBlock.append(saveBtn);
@@ -50,7 +40,8 @@ function addClass(cl) {
 
 function createWorkDay() {
   for (let i = 0; i < hours.length; i++) {
-    addHourRow(hours[i]);
+    addHourRow(hours[i], hours[i]);
+
     if (hours[i] < currentHour) {
       addClass("past");
     } else if (hours[i] === currentHour) {
