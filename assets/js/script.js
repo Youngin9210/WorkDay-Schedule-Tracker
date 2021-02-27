454f6f32a6d6b3ec3b5672c08a4de24bcab9f657
Kyle Young <kyleyoung.9210@gmail.com>
2021-02-27 15:11:45 +0000

    started commenting js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
+// declaring global variables
 let currentDay = $("#currentDay");
 let workDayContainer = $(".container");
+let currentHour;
+let timeBlock;
+let hourEl;
+let hourTask;
+let saveBtn;
+let saveIcon;
+
+// defining an array to be used later to compare to the hour of the day
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// defining an array of strings to input as text value in time blocks
 let workDayHours = [
   "9 am",
   "10 am",
@@ -12,49 +23,59 @@ let workDayHours = [
   "4 pm",
   "5 pm",
 ];
-let currentHour;
-let timeBlock;
-let hourEl;
-let hourTask;
-let saveBtn;
-let saveIcon;
 
-// handle displaying the date
+// handling displaying the date using moment.js
 function displayTime() {
+  // using moment.js to set current date format
   var rightNow = moment().format("dddd, MMMM Do");
+  // printing date onto page
   currentDay.text(rightNow);
 }
 
+// calling the current date and displaying it on the page
 displayTime();
 
+// getting the current hour of day using moment.js
 function now() {
+  // turning hour of day from string to number
   currentHour = Number.parseInt(moment().format("H"));
 }
 
-function addHourRow(hour, idHour) {
+// setting a function to create time block structure
+function addTimeBlock(hour, idHour) {
+  // creatiing timeblock div
   timeBlock = $("<div>").addClass("time-block row");
 
+  // creating an paragraph element to display the hour of the time block
   hourEl = $("<p>")
     .addClass("hour col-2 d-flex align-items-center justify-content-center")
     .attr("id", `taskHour${idHour}`)
     .text(hour);
 
+  // creating a textarea to use to input task
   hourTask = $(
     "<textarea placeholder='Enter task to be completed...'>"
   ).addClass("col-8 textarea description text-dark");
 
+  // creating a save button to save tasks
   saveBtn = $("<button>").addClass("saveBtn col-1");
 
+  // creating a save icon using bootstrap classes
   saveIcon = $("<i>").addClass("fas fa-save fa-2x");
 
-  function appendElements() {
-    workDayContainer.append(timeBlock);
-    timeBlock.append(hourEl, hourTask, saveBtn);
-    saveBtn.append(saveIcon);
-  }
-
+  // calling function to append elements to the html
   appendElements();
+
+  // calling function to add a click listener to the saveBtn to save the task into localStorage
   saveBtnClick();
+}
+
+// setting a function to append elements to html
+function appendElements() {
+  //
+  workDayContainer.append(timeBlock);
+  timeBlock.append(hourEl, hourTask, saveBtn);
+  saveBtn.append(saveIcon);
 }
 
 function addClass(cl) {
@@ -67,7 +88,7 @@ function addClass(cl) {
 function createWorkDay() {
   now();
   for (let i = 0; i < workDayHours.length; i++) {
-    addHourRow(workDayHours[i], i);
+    addTimeBlock(workDayHours[i], i);
     if (hours[i] < currentHour) {
       addClass("past");
     } else if (hours[i] === currentHour) {
@@ -102,4 +123,3 @@ function getTasks() {
 }
 
 createWorkDay();
-// getTasks();
